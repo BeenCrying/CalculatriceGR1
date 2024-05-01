157defb046e76dd5be0a3b545fc5c1fe0660fd3c
David G <david.giasson@claurendeau.qc.ca>
2024-05-01 12:02:18 -0400

    Ajout d'une barre de menu.

--- src/Calculatrice.java
+++ src/Calculatrice.java
@@ -1,12 +1,43 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 
 public class Calculatrice {
     public static void main(String[] args) {
-        Calculatrice calc = new Calculatrice();
         JFrame frame = new JFrame("Calculatrice");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 600);
         frame.setLocation(3200, 100);
+
+        JMenuBar menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+
+        JMenu menuEdit = new JMenu("Edit");
+        menuEdit.add(new JMenuItem("Copy"));
+        menuEdit.add(new JMenuItem("Paste"));
+        menuBar.add(menuEdit);
+
+        JMenu menuView = new JMenu("View");
+        ButtonGroup groupeMode = new ButtonGroup();
+
+        JMenuItem itemStandard = new JRadioButtonMenuItem("Standard");
+        itemStandard.setSelected(true);
+        groupeMode.add(itemStandard);
+        menuView.add(itemStandard);
+
+        JMenuItem itemScientific = new JRadioButtonMenuItem("Scientific");
+        groupeMode.add(itemScientific);
+        menuView.add(itemScientific);
+
+        menuView.addSeparator();
+        JMenuItem itemDigits = new JCheckBoxMenuItem("Digit Grouping");
+        menuView.add(itemDigits);
+        menuBar.add(menuView);
+
+        JMenu menuHelp = new JMenu("Help");
+        menuHelp.add(new JMenuItem("Help Topics"));
+        menuHelp.add(new JMenuItem("About Calculator"));
+        menuBar.add(menuHelp);
+
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
