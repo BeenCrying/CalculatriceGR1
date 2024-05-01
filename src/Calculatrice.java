0f32d5bdde830c3568a61b7aadba770a25eb0125
David G <david.giasson@claurendeau.qc.ca>
2024-05-01 11:21:18 -0400

    Création de la classe Calculatrice et d'un JFrame vide.

---
+++ src/Calculatrice.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Calculatrice {
+    public static void main(String[] args) {
+        Calculatrice calc = new Calculatrice();
+        JFrame frame = new JFrame("Calculatrice");
+        frame.setSize(500, 600);
+        frame.setLocation(3200, 100);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
